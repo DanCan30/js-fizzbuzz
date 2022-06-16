58ddd2a8a82034f8fcbf06a9f3b3312af25962dc
DanCan30 <danielecan3008@gmail.com>
2022-06-16 13:37:14 +0200

    FizzBuzz done

--- script/main.js
+++ script/main.js
@@ -1,5 +1,18 @@
 
     // Creating a standard for cycle from 1 to 100
     for( i = 1; i <= 100; i++) {
-        console.log(i);
+
+        // Setting a condition where if the index is a 3 or 5 multiplier the console.log will be different
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+
+        } else if (i % 5 == 0) {
+            console.log("Buzz")
+
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+            
+        } else {
+            console.log(i);
+        }
     }
