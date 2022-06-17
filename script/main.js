5397298588acbc6777ec7faf5ffb9f43c49bb70f
DanCan30 <danielecan3008@gmail.com>
2022-06-17 10:53:30 +0200

    MInor fixes

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 
 // Creating a standard for cycle from 1 to 100
-for( i = 1; i <= 100; i++) {
+for( let i = 1; i <= 100; i++) {
 
     // Initializing variables for the boxes
     let mainContainer = document.getElementById("container");
@@ -9,19 +9,19 @@ for( i = 1; i <= 100; i++) {
     box.classList.add("box");
     
     // Setting a condition where if the index is a 3 or 5 multiplier the console.log will be different
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
 
         // #1 Valorizing the variables and giving them css classes for style
         boxContent = "FizzBuzz";
         box.classList.add("fizzbuzz");
 
-    } else if (i % 5 == 0) {
+    } else if (i % 5 === 0) {
 
         // #2
         boxContent = "Buzz";
         box.classList.add("buzz");
 
-    } else if (i % 3 == 0) {
+    } else if (i % 3 === 0) {
 
         // #3
         boxContent = "Fizz";
